85bdb423e4b925f34d7ed5cd54c0e84f580c6c7b
Rozzzen <max2001zh@gmail.com>
2021-05-18 00:42:59 +0300

    implement jdbc template

--- src/main/java/com/zhuk/beautyshop/repo/ClientServiceJdbcTemplate.java
+++ src/main/java/com/zhuk/beautyshop/repo/ClientServiceJdbcTemplate.java
@@ -2,12 +2,15 @@ package com.zhuk.beautyshop.repo;
 
 import ch.qos.logback.core.net.server.Client;
 import com.zhuk.beautyshop.domain.shop.ClientService;
+import com.zhuk.beautyshop.domain.shop.ServiceCategory;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @AllArgsConstructor
@@ -15,7 +18,12 @@ public class ClientServiceJdbcTemplate {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public List<ClientService> findAll() {
-        return jdbcTemplate.query("SELECT * FROM services", new BeanPropertyRowMapper<>(ClientService.class));
+    public Map<ServiceCategory, List<ClientService>> findAll(String language) {
+        List<ClientService> clientServices = jdbcTemplate.query("SELECT * FROM services", new BeanPropertyRowMapper<>(ClientService.class));
+        clientServices.forEach(x -> {
+            if (language.equals("en")) x.setTitle(x.getTitleEn());
+            else if (language.equals("uk")) x.setTitle(x.getTitleUa());
+        });
+        return clientServices.stream().collect(Collectors.groupingBy(ClientService::getCategory));
     }
 }
